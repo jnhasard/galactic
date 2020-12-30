5c71a8fad16dac4a794fa98e2f65ece5959475e0
Jacques Hasard <jaxhei@Jacquess-MacBook-Pro.local>
2020-12-30 17:10:57 -0300

    deploy: try 3

--- galactic_salesman/settings.py
+++ galactic_salesman/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 import os
 
@@ -129,3 +130,4 @@ STATICFILES_DIRS = (
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
