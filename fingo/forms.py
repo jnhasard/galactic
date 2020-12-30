7eaf71542a2ba13adc33ecd7c6ef152784adc60b
Jacques Hasard <jaxhei@Jacquess-MacBook-Pro.local>
2020-12-30 18:17:14 -0300

    styling

--- fingo/forms.py
+++ fingo/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class MyForm(forms.Form):
     info = forms.CharField(widget=forms.Textarea(
-        attrs={"rows": 16, "cols": 100}), label="")
+        attrs={"rows": 16, "cols": 60}), label="")
